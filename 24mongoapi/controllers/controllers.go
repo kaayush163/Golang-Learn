8be71b132719c327b3bad0e586c3101dd1ac70b4
kaayush163 <kaayush163>
2024-05-05 19:51:45 +0530

    Json encode fucntion written for again all methods Update,delete,create

--- 24mongoapi/controllers/controllers.go
+++ 24mongoapi/controllers/controllers.go
@@ -1,10 +1,14 @@
 package controller
 
+//first letter is always smaller of whatever we havve created whether controller or db
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -107,7 +111,7 @@ func deleteOneMovie(movieId string) {
 }
 
 // delete all record s from mongodb
-func deleteAllMovie() {
+func deleteAllMovie() int64 {
 	// filter := bson.D{{}} //seniors developers dont like in different line to write instead do in collection.DeleteMany(context,bson.D({})) directly
 
 	// collection.DeleteMany(context.Background(), filter)
@@ -122,4 +126,86 @@ func deleteAllMovie() {
 
 	return deleteResult.DeletedCount
 
+}
+
+func getAllMovies() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cur.Next(context.Background()) {
+		var movie bson.M
+
+		err := cur.Decode(&movie)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		movies = append(movies, movie)
+	}
+
+	defer cur.Close(context.Background())
+	return movies
+}
+
+// Actusl controller = file
+// get all movies from databsse in golang
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request) { //need to inserted into router so important to mention formal parameter w and r
+
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+
+	allMovies := getAllMovies()
+
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie) //like we do in ajvavsvripty res.json()
+
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r) //from request body we get every params easily
+	updateOneMovie(params["id"])
+
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	count := deleteAllMovie()
+
+	json.NewEncoder(w).Encode(count)
 }
